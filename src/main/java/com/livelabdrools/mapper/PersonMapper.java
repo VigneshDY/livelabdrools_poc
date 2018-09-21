1004f5f89473ef1da18717b2f792dbf8abb60e22
Seth <674448@cognizant.com>
2018-09-21 20:12:07 +0530

    mapping done

--- src/main/java/com/livelabdrools/mapper/PersonMapper.java
+++ src/main/java/com/livelabdrools/mapper/PersonMapper.java
@@ -10,31 +10,44 @@ import com.livelabdrools.reader.DelimiterReader;
 
 public class PersonMapper extends DataMapper{
 
-	List<Person> listPerson=new ArrayList<Person>();
-	private DelimiterReader delimiterReader;
-	public List getData(String line) {
-		// TODO Auto-generated method stub
-		String temp[]=new String[5];
-		Map<String,String> map=new HashMap<String,String> ();
-		String header=delimiterReader.getHeader();
-		String arr[]=header.split("\\|");
-		Integer length = 0;
-		String count = "0";
-		for (int j = 0; j < arr.length; j++) {
-			map.put(arr[j], count);
-			length++;
-			count = length.toString();
+	@Override
+	public List<Object> getData(List<String[]> header, List<String[]> data) {
+		List<Object> person=new ArrayList<Object> ();
+		Map<String,Integer> map=new HashMap<String,Integer> ();
+		int j=0;
+		for(String[] head:header)
+		{
+			for(int i=0;i<head.length;i++)
+			{
+				map.put(head[i], j);
+				j++;
+			}
+			
 		}
-		String arr1[] = line.split("\\|");
-		Person person1 = new Person();
-		// int k = 0;
-		person1.setId(arr1[Integer.parseInt(map.get(temp[0]))]);
-		person1.setFirstName(arr1[Integer.parseInt(map.get(temp[1]))]);
-		person1.setLastName(arr1[Integer.parseInt(map.get(temp[2]))]);
-		person1.setLocation(arr1[Integer.parseInt(map.get(temp[3]))]);
-		person1.setTimeZone(null);
-		listPerson.add(person1);
-		return listPerson;
-	}
+		String header1[]=new String[j];
+		int index=0;
+		
+		for(Map.Entry<String, Integer> m:map.entrySet())
+		{
+			header1[index]=m.getKey();
+		}
+		for(String[] data1:data)
+		{
+			for(int i=0;i<data1.length;i++)
+			{
+				Person person1=new Person();
+				person1.setId(data1[map.get(header1[i])]);
+				person1.setFirstName(data1[map.get(header1[i+1])]);
+				person1.setLastName(data1[map.get(header1[i+2])]);
+				person1.setLocation(data1[map.get(header1[i+3])]);
+				person1.setTimeZone(null);
+				person.add(person1);
+				break;
+			}
+		}
+          return person;
+				
+			}
+
 
 }
